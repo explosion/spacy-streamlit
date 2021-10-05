45db9986e0c0204e9ad2bc9fbba0d1688dec3518
Henriette Behr <henriette.behr@web.de>
2021-10-05 13:55:47 +0200

    Deleted test.py (used for running streamlit)

--- test.py
+++
@@ -1,5 +0,0 @@
-import spacy_streamlit
-
-models = ["en_core_web_md","en_core_web_sm"]
-default_text = "Sundar Pichai is the CEO of Google."
-spacy_streamlit.visualize(models, default_text, show_visualizer_select=True)
