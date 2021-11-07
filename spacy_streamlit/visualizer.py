8d00ca834cd10da4aabacfad639c3d9831195b45
callistachang <34191886+callistachang@users.noreply.github.com>
2021-11-08 00:27:48 +0800

    Error checking
    Error 1: When manual=True but show_table=True
    Error 2: When manual=True but doc is not of type 'list'

--- spacy_streamlit/visualizer.py
+++ spacy_streamlit/visualizer.py
@@ -162,7 +162,7 @@ def visualize_parser(
 
 
 def visualize_ner(
-    doc: Union[spacy.tokens.Doc, dict],
+    doc: Union[spacy.tokens.Doc, List[Dict[str, str]]],
     *,
     labels: Sequence[str] = tuple(),
     attrs: List[str] = NER_ATTRS,
@@ -176,8 +176,14 @@ def visualize_ner(
     if title:
         st.header(title)
 
-    labels = labels or [ent.label_ for ent in doc.ents]
-
+    if manual:
+        if show_table:
+            st.warning("When the parameter 'manual' is set to True, the parameter 'show_table' must be set to False.")
+        if not isinstance(doc, list):
+            st.warning("When the parameter 'manual' is set to True, the parameter 'doc' must be of type 'list', not 'spacy.tokens.Doc'.")
+    else:
+        labels = labels or [ent.label_ for ent in doc.ents]
+ 
     if not labels:
         st.warning("The parameter 'labels' should not be empty or None.")
     else:
