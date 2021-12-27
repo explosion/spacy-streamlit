1ab801d3b8b5cc9108c5cce8bd1b10c90c4a7c4d
Narayan Acharya <narayan.acharya6@gmail.com>
2021-12-27 10:36:52 -0500

    Update spacy_streamlit/visualizer.py
    Co-authored-by: Sofie Van Landeghem <svlandeg@users.noreply.github.com>

--- spacy_streamlit/visualizer.py
+++ spacy_streamlit/visualizer.py
@@ -187,7 +187,7 @@ def visualize_ner(
     key (str): Key used for the streamlit component for selecting labels.
     manual (bool): Flag signifying whether the doc argument is a Doc object or a List of Dicts containing entity span
     information.
-    options: Dictionary of options to be passed to the displacy render method for generating the HTML to be rendered.
+    displacy_options: Dictionary of options to be passed to the displacy render method for generating the HTML to be rendered.
     """
     if colors:
         if not options:
